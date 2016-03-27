5593f7e6fbf558e08446cb657bd0a35085e964c2
Misha Wagner <mishajw@gmail.com>
2016-03-27 18:14:19 +0000

    Initial setup for ScalaJS

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "D3ScalaJS"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+
+enablePlugins(ScalaJSPlugin)
+
+scalaJSUseRhino in Global := false
---
+++ src/main/scala/TestApp.scala
@@ -0,0 +1,12 @@
+import scala.scalajs.js.JSApp
+import scala.scalajs.js.annotation.JSExport
+
+/**
+  * Created by misha on 27/03/16.
+  */
+object TestApp extends JSApp {
+  @JSExport
+  override def main(): Unit = {
+    println("Hello, world")
+  }
+}
