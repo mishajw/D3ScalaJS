b25ef7839224be42c09fba0111522c5926c8aa7a
Misha Wagner <mishajw@gmail.com>
2016-03-27 18:44:12 +0000

    Interactive button

--- src/main/scala/TestApp.scala
+++ src/main/scala/TestApp.scala
@@ -7,6 +7,13 @@ object TestApp extends JSApp {
   @JSExport
   override def main(): Unit = {
     showMessage("Hello, jQuery!")
+
+    jQuery("body")
+      .append(
+        jQuery("<button>Press me</button>")
+            .attr("id", "main-button")
+            .click(() => showMessage("Pressed a button"))
+      )
   }
 
   def showMessage(message: String) = {
